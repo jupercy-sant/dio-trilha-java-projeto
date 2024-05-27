21353a03b2b00cf4c9964b2c89330d282838db0f
jupercy-sant <juper.sant@gmail.com>
2024-05-27 09:49:58 -0300

    Comitando mudanças

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,15 @@
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+       
+        // TODO: conhecer e importar a classe scanner
+
+        // Exibir as mensagens para o nosso usuário
+
+        // Obter pela classe scanner os valores digitados no terminal
+
+        // Exibir a mensagem conta criada
+
+        
+
     }
 }
